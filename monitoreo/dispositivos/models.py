a905039d7bbbeedd7c9cd50460e9b6eb14e6a1a6
Pedro Gómez <pedrogomezzambra182@gmail.com>
2025-08-28 17:53:02 -0400

    he creado la rama donde trabaje en categoria y zona

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-class Dispositivo(models.Model):
-    nombre = models.CharField(max_length=100)
-    consumo = models.IntegerField()
-    estado = models.BooleanField(default=True)
-    
+
+x
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True, help_text="Descripción de la categoría.")
+
     def __str__(self):
         return self.nombre
+
+
+class Zona(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True, help_text="Descripción de la zona.")
+
+    def __str__(self):
+        return self.nombre
+
